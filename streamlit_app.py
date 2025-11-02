2e3da7258cfffd35d72af5af9ae05e6465e6d05d
Jeeva1112 <67320661+Jeeva1112@users.noreply.github.com>
2025-11-02 23:22:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from streamlit_option_menu import option_menu
-
+from streamlit_navigation_bar import st_navbar
 # ---------------------- PAGE CONFIG ----------------------
 st.set_page_config(page_title="Digital Finance Chart Studio", layout="wide")
 
