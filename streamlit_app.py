f286e168861c5a12d6236e1c5b3e0384c53bb2c2
Jeeva1112 <67320661+Jeeva1112@users.noreply.github.com>
2025-11-02 21:45:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ main[data-testid="stAppViewContainer"] {
 with st.container():
     col1, col2 = st.columns([0.15, 0.65])
     with col1:
-        st.image("philips.svg", width=70;)
+        st.image("philips.svg", width=70)
     with col2:
         st.markdown("<h2 style='color:white;margin-top:7px;'>📊 Digital Finance Chart Studio</h2>", unsafe_allow_html=True)
 
